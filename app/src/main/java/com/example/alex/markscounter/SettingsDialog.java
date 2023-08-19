c2eb1af93f05dc7642bc66585cd2fcd69a791daf
Alexander Zhdanov <qwert2603@mail.ru>
2023-08-19 23:03:24 +0500

    remove about_message

--- app/src/main/java/com/example/alex/markscounter/SettingsDialog.java
+++ app/src/main/java/com/example/alex/markscounter/SettingsDialog.java
@@ -8,11 +8,6 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.style.URLSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
@@ -32,12 +27,6 @@ public class SettingsDialog extends DialogFragment {
         int textViewColor = getResources().getColor(MainActivity.isDarkTheme ? R.color.text_night : R.color.text_day);
         view.<TextView>findViewById(R.id.title_text_view).setTextColor(textViewColor);
 
-        TextView messageTextView = view.findViewById(R.id.message_text_view);
-        messageTextView.setTextColor(textViewColor);
-        messageTextView.setText(getAboutMessage());
-        messageTextView.setMovementMethod(LinkMovementMethod.getInstance());
-        messageTextView.setHighlightColor(Color.TRANSPARENT);
-
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         CheckBox mark1CheckBox = view.findViewById(R.id.mark_1_CheckBox);
         mark1CheckBox.setTextColor(textViewColor);
@@ -51,24 +40,5 @@ public class SettingsDialog extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .create();
-    }
-
-    private CharSequence getAboutMessage() {
-        SpannableStringBuilder spannable = SpannableStringBuilder.valueOf(getString(R.string.about_message));
-
-        spannable.setSpan(
-                new URLSpan("https://www.shutterstock.com/ru/g/Annvzhdanova") {
-                    @Override
-                    public void updateDrawState(TextPaint ds) {
-                        super.updateDrawState(ds);
-                        ds.setUnderlineText(false);
-                    }
-                },
-                spannable.length() - 6,
-                spannable.length() - 1,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        );
-
-        return spannable;
     }
 }
